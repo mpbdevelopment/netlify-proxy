b0f1381898d939fdd0e63dd35d3e78d50e1dc249
mpbdevelopment <159507682+mpbdevelopment@users.noreply.github.com>
2025-02-21 19:40:15 -1000

    Update chargeSubscription.js

--- netlify/functions/chargeSubscription.js
+++ netlify/functions/chargeSubscription.js
@@ -10,9 +10,7 @@ const CORS_HEADERS = {
 
 let app;
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString()
-  );
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
   app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
