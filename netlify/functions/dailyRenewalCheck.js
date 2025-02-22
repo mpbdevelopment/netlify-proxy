fc64c20fbe097f7b55dc4143e57a14ea6fdeede0
mpbdevelopment <159507682+mpbdevelopment@users.noreply.github.com>
2025-02-21 19:40:03 -1000

    Update dailyRenewalCheck.js

--- netlify/functions/dailyRenewalCheck.js
+++ netlify/functions/dailyRenewalCheck.js
@@ -13,9 +13,7 @@ const CORS_HEADERS = {
 
 let app;
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString()
-  );
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
   app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
